a708eb33bae3fe7ff87b33a15226e8423b41bf94
Maxime Kiriakov <maxime.kiriakov@erlang-solutions.com>
2022-09-15 07:59:18 +0300

    block2: integration.yaml naming and ebank supervisor added

--- src/ebank_sup.erl
+++ src/ebank_sup.erl
@@ -1,37 +1,48 @@
-%%%-------------------------------------------------------------------
+%%%------------------------------------------------------------
 %% @doc ebank top level supervisor.
 %% @end
-%%%-------------------------------------------------------------------
+%%%------------------------------------------------------------
 
 -module(ebank_sup).
 
 -behaviour(supervisor).
 
--export([start_link/0]).
+-export([
+    start_atm/0
+]).
 
--export([init/1]).
+-export([
+    init/1
+]).
 
 -define(SERVER, ?MODULE).
 
-start_link() ->
+start_atm() ->
     supervisor:start_link({local, ?SERVER}, ?MODULE, []).
 
-%% sup_flags() = #{strategy => strategy(),         % optional
-%%                 intensity => non_neg_integer(), % optional
-%%                 period => pos_integer()}        % optional
-%% child_spec() = #{id => child_id(),       % mandatory
-%%                  start => mfargs(),      % mandatory
-%%                  restart => restart(),   % optional
-%%                  shutdown => shutdown(), % optional
-%%                  type => worker(),       % optional
-%%                  modules => modules()}   % optional
 init([]) ->
     SupFlags = #{
-        strategy => one_for_all,
+        strategy => one_for_one,
         intensity => 0,
         period => 1
     },
-    ChildSpecs = [],
-    {ok, {SupFlags, ChildSpecs}}.
 
-%% internal functions
+    ChildSpecs = [
+        #{
+            id => backend,
+            start => {atm, start_link, []},
+            restart => permanent,
+            shutdown => brutal_kill,
+            type => worker,
+            modules => [backend]
+        },
+        #{
+            id => atm,
+            start => {atm, start_link, [atm1]},
+            restart => permanent,
+            shutdown => brutal_kill,
+            type => worker,
+            modules => [atm]
+        }
+    ],
+    {ok, {SupFlags, ChildSpecs}}.
